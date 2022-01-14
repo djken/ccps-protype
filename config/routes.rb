ffbbb2480d1b7c49efa0049c1db710bd6ceeee72
Jean-Kenel <jeankeneld@gmail.com>
2022-01-13 22:52:18 -0500

    Create Class Group

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,5 @@ Rails.application.routes.draw do
   resources :salles
   resources :sessionencours
 
-  get 'classes', to: 'horaires#groupeclasses'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
